0b374e90f203ed9ba223b72134c9d3e03013f158
zied jmal <zied.jmal@enis.tn>
2024-07-19 16:43:20 +0200

    version 1.0 when push to gitlab

--- src/services/database_services/mongo_service_with_many_data_schemas_and_specifics_topics.py
+++ src/services/database_services/mongo_service_with_many_data_schemas_and_specifics_topics.py
@@ -9,7 +9,9 @@ from src.modules.database_modules.database import (
 import logging
 
 # Suppress debug messages from MongoDB driver
-logging.getLogger('pymongo').setLevel(logging.WARNING)
+logging.getLogger("pymongo").setLevel(logging.WARNING)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class DataWriterService:
     def __init__(self):
@@ -28,7 +30,7 @@ class DataWriterService:
 
         # Check if the collection already exists
         if collection_name in db.list_collection_names():
-            print(
+            logger.info(
                 f"Collection '{collection_name}' already exists in database '{db_name}'. Skipping creation."
             )
             return
@@ -42,11 +44,11 @@ class DataWriterService:
                     "granularity": granularity,
                 },
             )
-            print(
+            logger.info(
                 f"Created time-series collection '{collection_name}' in database '{db_name}'."
             )
         except Exception as e:
-            print(
+            logger.info(
                 f"Failed to create collection '{collection_name}' in database '{db_name}': {e}"
             )
 
@@ -82,43 +84,67 @@ class DataWriterService:
             return self.evaluate_expression(data, template)
         else:
             return template
-
+    def save_metadata(self, db_name: str, collection_name: str, metadata: dict):
+        try:
+            metadata_collection = self.client["metadata"][db_name]
+            logger.info(f"metadata collection: {metadata_collection}")
+            # Define the query to find an existing document by collection_name
+            query = {"collection_name": collection_name}
+            logger.info(f"query: {query}")
+            # Define the update to apply to the found document
+            update = {
+                "$set": {
+                    "metadata": metadata,
+                }
+            }
+            # Update the document if it exists, otherwise insert a new one
+            result = metadata_collection.find_one_and_update(
+                query,
+                update,
+                upsert=True  # Create a new document if none exists
+            )
+            
+            if result.matched_count:
+                logger.info(f"Updated metadata for collection '{collection_name}' in {db_name}/metadata.")
+            else:
+                logger.info(f"Inserted new metadata for collection '{collection_name}' in {db_name}/metadata.")
+            
+        except Exception as e:
+            logger.error(f"Failed to save or update metadata: {e}")
     def write_data(self, topic, message):
         self.schemas_cursor = self.client["schemas_config4"]["schemas"].find()
-        self.schemas = list(
-            self.schemas_cursor
-        )  
+        self.schemas = list(self.schemas_cursor)
         try:
             topic_parts = topic.split("/")
-            #database_name = topic_parts[0]
-            #device_name = topic_parts[1]
-            
+            # database_name = topic_parts[0]
+            # device_name = topic_parts[1]
+
             # # Find the schema for the database_name (formerly database)
             # schema = next(
             #     (s for s in self.schemas if s["database"] == database_name), None
             # )
             # if not schema:
             #     return
-            actual_schema=None
+            actual_schema = None
             # collection_names = schema["sub_schemas"]
-            found_the_device =False
-            schemas=self.schemas
+            found_the_device = False
+            schemas = self.schemas
             for topic_part in topic_parts:
                 for schema in schemas:
                     for sub_schema in schema["sub_schemas"]:
                         if topic_part in sub_schema["devices"]:
-                            #print("sub_schema : ",sub_schema)
-                            #print(f"""sub_schema["devices"] :{sub_schema["devices"]}""")
-                            #print("topic_part",topic_part)
+                            # logger.info("sub_schema : ",sub_schema)
+                            # logger.info(f"""sub_schema["devices"] :{sub_schema["devices"]}""")
+                            # logger.info("topic_part",topic_part)
                             device_name = topic_part
-                            database_name=topic_part
-                            actual_schema=sub_schema
-                            found_the_device=True
-                            print("OKKKKKKKK")
+                            database_name = topic_part
+                            actual_schema = sub_schema
+                            found_the_device = True
+                            logger.info("OKKKKKKKK")
                             break
-                    if  found_the_device:
+                    if found_the_device:
                         break
-                if  found_the_device:
+                if found_the_device:
                     break
             if not found_the_device:
                 return
@@ -127,11 +153,11 @@ class DataWriterService:
             collection_name = None
             path_mapping = None
             data_mapping = None
-            print("device_name:",device_name)
-            print("actual_schema",actual_schema)
-            print(f"""this device {device_name} in {actual_schema["devices"]}""")
+            logger.info("device_name:", device_name)
+            logger.info("actual_schema", actual_schema)
+            logger.info(f"""this device {device_name} in {actual_schema["devices"]}""")
             for topic_pattern in actual_schema["topics"]:
-                print(f"topic asked for {topic_pattern} and topic received {topic}")
+                logger.info(f"topic asked for {topic_pattern} and topic received {topic}")
                 if mqtt.topic_matches_sub(topic_pattern, topic):
                     collection_name = actual_schema["collection"]
                     path_mapping = actual_schema.get("path_mapping", {})
@@ -140,22 +166,22 @@ class DataWriterService:
 
             if not collection_name:
                 return
-            print("collection_name : ",collection_name)
+            logger.info("collection_name : ", collection_name)
             raw_data = json.loads(message)
-            print(f"Raw data: {raw_data}")
+            logger.info(f"Raw data: {raw_data}")
             specific_collection_name = ""
             transformed_data = {}
             for key, path_expr in data_mapping.items():
-                print(f"Evaluating path expression: {path_expr}")
-                print(f"Key: {key}")
-                print(f"transformed_data: {transformed_data}")
-                print(f"data_mapping: {data_mapping}")
+                logger.info(f"Evaluating path expression: {path_expr}")
+                logger.info(f"Key: {key}")
+                logger.info(f"transformed_data: {transformed_data}")
+                logger.info(f"data_mapping: {data_mapping}")
                 if key in path_mapping:
                     path_name = self.evaluate_expression(raw_data, path_mapping[key])
                     specific_collection_name = (
-                        f"{collection_name}|{device_name}|{path_name}"
+                        f"{path_name}"
                     )
-                    print(f"Specific collection name: {specific_collection_name}")
+                    logger.info(f"Specific collection name: {specific_collection_name}")
                     if (
                         specific_collection_name
                         not in self.client[database_name].list_collection_names()
@@ -174,11 +200,11 @@ class DataWriterService:
                         raw_data, path_expr
                     )
                 elif "subpath" in path_mapping:
-                    path_name = self.evaluate_expression(raw_data, path_mapping["subpath"])
-                    specific_collection_name = (
-                        f"{device_name}|{path_name}"
+                    path_name = self.evaluate_expression(
+                        raw_data, path_mapping["subpath"]
                     )
-                    print(f"Specific collection name: {specific_collection_name}")
+                    specific_collection_name = f"{path_name}"
+                    logger.info(f"Specific collection name: {specific_collection_name}")
                     if (
                         specific_collection_name
                         not in self.client[database_name].list_collection_names()
@@ -193,18 +219,19 @@ class DataWriterService:
                         raw_data, path_expr
                     )
                 else:
-                    print(f"No path mapping found for '{key}'. Skipping...")
+                    logger.info(f"No path mapping found for '{key}'. Skipping...")
 
             if specific_collection_name == "":
                 specific_collection_name = f"{device_name}"
-                print("specific_collection_name:", specific_collection_name)
+                logger.info("specific_collection_name:", specific_collection_name)
                 self.create_time_series_collection(
                     database_name, specific_collection_name, "timestamp", "metadata"
                 )
-            print("specific_collection_name")
+            logger.info("specific_collection_name")
             metadata = {
                 "database": database_name,
                 "type": collection_name,
+                "collection_name": specific_collection_name,
                 "device_name": device_name,
                 "path": topic,
             }
@@ -213,13 +240,13 @@ class DataWriterService:
                 "metadata": metadata,
                 "data": transformed_data,
             }
-            print(f"Metadata: {metadata}")
-            print(f"Document to insert: {document}")
+            logger.info(f"Metadata: {metadata}")
+            logger.info(f"Document to insert: {document}")
             self.client[database_name][specific_collection_name].insert_one(document)
-            print(
-                f"Inserted document into {database_name}/{device_name}: {document}"
-            )
+            logger.info(f"Inserted document into {database_name}/{specific_collection_name}: {document}")
+            # Save metadata to the metadata database
+            logger.info(f"Saving metadata to {database_name}")
+            self.save_metadata(db_name=database_name,collection_name=specific_collection_name, metadata=metadata)
 
         except Exception as e:
-            print(f"Error processing message: {e}")
-
+            logger.info(f"Error processing message: {e}")
